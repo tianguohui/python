5362f5780aa68c633fc8a61bd0110d29fed09f72
tianguohui <1450046897@qq.com>
2020-08-28 13:14:29 +0800

    增加run_case基本方法

--- requests/Run/run_main.py
+++ requests/Run/run_main.py
@@ -1,3 +1,22 @@
 #coding = utf-8
 from Excel_case.excel_def_case import Handexcel
+from Base_request.base_request import request
 import os
+
+class RunMain:
+    def run_main(self):
+        max_rows = Handexcel.get_row_max()
+        for row in range(max_rows):
+            data = Handexcel.get_row_value(row+2)
+            data_method = data[4]
+            data_url = data[3]
+            data_data = data[5]
+            is_run = data[2]
+            if is_run == 'yes':
+                request.send_main(data_method,data_url,data_data)
+            else:
+                pass
+
+if __name__ == '__main__':
+    run = RunMain()
+    run.run_main()
